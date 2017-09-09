8a4166374e0f06fdbf13522ee7d6f9f218b61616
Michael Bøcker-Larsen <mblarsen@gmail.com>
2017-09-09 18:12:52 +0800

    Added EditModal

---
+++ src/EditModal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="edit-modal">
+    <div class="modal fade" :class="{'is-active': open}">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <slot name="title">Edit</slot>
+          </div>
+          <div class="modal-body edit-modal__modal-body">
+            <slot name="body"></slot>
+          </div>
+          <div class="modal-footer">
+            <slot name="footer"></slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'edit-modal',
+  props: ['open'],
+}
+</script>
+
