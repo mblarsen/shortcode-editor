a6d6372c7b35d4f1d202947a2791e9e4e5860737
Michael Bøcker-Larsen <mblarsen@gmail.com>
2017-09-09 18:17:43 +0800

    Empty text tokens is handled by shortcode-tokenizer now

--- src/EditorFactory.js
+++ src/EditorFactory.js
@@ -10,28 +10,24 @@ class Editor {
   }
 }
 
-export function wrap(token, index) {
-  token.index = index
-  if (token.type === Tokenizer.TEXT) {
-    return new Editor('text-editor', token)
-  }
-  return new Editor(`${token.name}-editor`, token)
-}
-
 export default function create(content) {
   let tokens = content
   if (typeof content === 'string') {
-    const tokenizer = new Tokenizer()
-    tokenizer.strict = false
+    const tokenizer = new Tokenizer(null, {
+      strict: false,
+      skipWhiteSpace: true
+    })
     tokens = tokenizer
       .input(content)
       .ast()
   }
   return tokens
     .filter(token => token.type !== Tokenizer.ERROR)
-    .filter(token => {
-      if (token.type !== Tokenizer.TEXT) { return true }
-      return token.body.replace(/\s/g, '').length > 0
+    .map((token, index) => {
+      token.index = index
+      if (token.type === Tokenizer.TEXT) {
+        return new Editor('text-editor', token)
+      }
+      return new Editor(`${token.name}-editor`, token)
     })
-    .map(wrap)
 }
