96b6a0609e135e81226f4a1879cf507d5b65381b
Michael Bøcker-Larsen <mblarsen@gmail.com>
2017-09-09 18:17:02 +0800

    Added save button and log to front

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="slot-editor">
     <div class="row">
       <div class="col-xs-8 col-xs-offset-2">
-        <textarea v-model="content"></textarea>
+        <textarea v-model="content" style="width: 100%"></textarea>
       </div>
     </div>
     <div class="row">
@@ -16,10 +16,13 @@
     </div>
     <div class="row">
       <div class="col-xs-8 col-xs-offset-2">
-        <add-button context="all" :caller="callerId"/>
+        <add-button context="root" :caller="callerId"/>
       </div>
     </div>
     <catalog/>
+      <hr />
+    <save-button @save="save"></save-button>
+    <div v-for="item in log">{{item}} <button class="btn btn-sm" @click="content = item">use</button></div>
   </div>
 </template>
 
@@ -30,7 +33,9 @@ import createEditors from '@/EditorFactory'
 import TextEditor from '@/editors/Text'
 import RowEditor from '@/editors/Row'
 import Catalog from '@/Catalog'
+import ProductListEditor from '@/editors/ProductList'
 import AddButton from '@/AddButton'
+import SaveButton from '@/SaveButton'
 
 export default {
   name: 'slot-editor',
@@ -39,14 +44,17 @@ export default {
   },
   components: {
     AddButton,
+    SaveButton,
     Catalog,
     CodeSlot,
     RowEditor,
     TextEditor,
+    ProductListEditor,
   },
   data() {
     return {
       content: '',
+      log: [],
     }
   },
   computed: {
@@ -59,10 +67,24 @@ export default {
     }
   },
   created() {
+    /* prepend root items */
     this._provided.bus.$on(this.callerId, (payload) => {
       this.content += payload.item
+      this.$nextTick(this.save)
     })
+    /* on update save */
+    this._provided.bus.$on('update', this.save)
   },
+  methods: {
+    save() {
+      const template = this.$children
+        .filter(c => c.$options.name.endsWith('-editor'))
+        .map(c => c.toTemplate())
+        .join('')
+      this.content = template
+      this.log.unshift(template)
+    }
+  }
 }
 </script>
 <style>
---
+++ src/SaveButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="save-button">
+    <button @click="$emit('save')" class="editor-add btn btn-success">
+      <span>Save</span>
+    </button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'save-button',
+}
+</script>
