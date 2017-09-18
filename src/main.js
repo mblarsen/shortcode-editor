ac47113ca17519c7bc55c0c88bba182358a33964
Michael Bøcker-Larsen <mblarsen@gmail.com>
2017-09-18 18:20:29 +0800

    feat: Editors are as their placeholders are added to the dom

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 require('@/css')
 import Vue from 'vue'
 import App from './App'
+import sentinel from 'sentinel-js'
 
 Vue.config.productionTip = false
 
-Array.from(document.getElementsByClassName('slot-editor'))
-  .map(el => {
-    return new Vue({el, render: h => h(App)})
-  })
+sentinel.on('.slot-editor', (el) => {
+  return new Vue({el, render: h => h(App)})
+})
